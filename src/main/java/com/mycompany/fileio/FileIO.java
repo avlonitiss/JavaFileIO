d2746c3cf26a9400e71c03b0cf4056c409ac8ec2
Spyros <Spyros@Markos>
2022-01-07 23:35:28 +0200

    Files Read Write Lesson Secon Lab with BufferedWriter object but with constructor error

---
+++ src/main/java/com/mycompany/fileio/FileIO.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.fileio;
+
+import java.io.BufferedWriter;
+
+/**
+ *
+ * @author Spyros
+ */
+public class FileIO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // First Lab in writing to files
+        // BufferedWriter Object
+        // Add import java.io.BufferedWriter
+        // But still complains about constructor
+        
+        BufferedWriter writer = new BufferedWriter();
+        
+    }
+    
+}
