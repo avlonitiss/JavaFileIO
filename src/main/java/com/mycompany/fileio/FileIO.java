ef07016fb240959839f54b57cb3bdae55e41408b
Spyros <Spyros@Markos>
2022-01-07 23:45:27 +0200

    Files Read Write Lesson forth Lab with try catch add a new line to file add writer.close method

--- src/main/java/com/mycompany/fileio/FileIO.java
+++ src/main/java/com/mycompany/fileio/FileIO.java
@@ -6,6 +6,9 @@ package com.mycompany.fileio;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,18 +20,33 @@ public class FileIO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        // First Lab in writing to files
-        // BufferedWriter Object
-        // Add import java.io.BufferedWriter
-        // But still complains about constructor
-        //
-        // third lab adds constructor and FileWriter Object
-        // add import for FileWriterS
-        // and also add filename with no path
-        // but still errors
-        
-        BufferedWriter writer = new BufferedWriter(new FileWriter("myfile.txt"));
+        try {
+            // TODO code application logic here
+            // First Lab in writing to files
+            // BufferedWriter Object
+            // Add import java.io.BufferedWriter
+            // But still complains about constructor
+            //
+            // third lab adds constructor and FileWriter Object
+            // add import for FileWriterS
+            // and also add filename with no path
+            // but still 
+            //
+            // forth lab add the try/catch block
+            // add a line to new file
+            // and
+            // add the writer.close() method to save the file
+            
+          
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter("myfile.txt"));
+            writer.write("my first data");
+            writer.close();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(FileIO.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        }
         
     }
     
